540577bdea7a4be38ece6fef0ece2935db62c77b
Wildan Zulfikar <wildanzulfikar811@gmail.com>
2026-05-21 23:21:10 +0700

    change wa

--- src/Contact/ListContact.jsx
+++ src/Contact/ListContact.jsx
@@ -20,7 +20,7 @@ const ListContact = () => {
         {/* Phone */}
         <div className="flex flex-col items-center text-gray-700">
           <FaUserAlt className="text-2xl mb-2" />
-          <p>+62 8131 8130 037</p>
+          <p>+62 899 9900 311</p>
         </div>
 
         {/* Email */}
